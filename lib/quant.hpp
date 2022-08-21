8e849b4ce80f247fe0b0d83600b6d7bc66004955
junyoung-sim <jysim.ny@gmail.com>
2022-08-21 12:37:18 -0400

    v7

--- lib/quant.hpp
+++ lib/quant.hpp
@@ -32,10 +32,10 @@ public:
     Quant() {}
     Quant(std::vector<Market> &_market_dataset, std::string _checkpoint): checkpoint(_checkpoint) {
         market_dataset = &_market_dataset;
-        look_back = 25;
+        look_back = 50;
         action_space = std::vector<double>({-1.0, 0.0, 1.0}); // short, idle, long
 
-        init({{100,100},{100,100},{100,100},{100,100},{100,100},{100,3}});
+        init({{200,200},{200,200},{200,200},{200,200},{200,100},{100,3}});
         load();
 
         // --- //
