e1fd5de30cdce7e976f94c21dc775f56bbf0a8bd
junyoung-sim <jysim.ny@gmail.com>
2022-09-06 23:31:45 -0400

    v10 ready for final code check

--- data/download.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import yfinance as yf
-
-tickers = [ticker[:-1] for ticker in open("tickers").readlines()]
-
-for ticker in tickers:
-    yf.download(ticker).to_csv("{}.csv" .format(ticker))
