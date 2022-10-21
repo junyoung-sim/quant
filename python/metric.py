441324267110134221498a845dd1f1f6790057d9
junyoung-sim <jysim.ny@gmail.com>
2022-10-21 00:09:13 -0400

    2022-10-20

--- python/metric.py
+++ python/metric.py
@@ -2,11 +2,9 @@
 
 import sys
 import math
+import numpy as np
 
-def annualized_return(history):
-    hist = history.copy()
-    hist.insert(0, 1.00)
-
+def annualized_return(hist):
     years = 0
     expectation = 1.00
     returns = []
@@ -23,12 +21,23 @@ def annualized_return(history):
         returns.append(ret)
 
     annualized = math.pow(expectation, 1 / years) - 1.00
-
     return annualized, returns
+
+def maximum_drawdown(hist):
+    mdd = 0.00
+    t1, t2 = 0, 1
+    while t1 < len(hist):
+        while (t2 < len(hist) - 1) & (hist[t1] == max(hist[t1:t2+1])):
+            t2 += 1
+        dd = (min(hist[t1:t2+1]) - hist[t1]) / hist[t1]
+        mdd = min(dd, mdd)
+        t1 = t2
+        t2 = t1 + 1
+    return mdd
 
 def main():
     ticker = sys.argv[1]
-    benchmark, model, action = [], [], []
+    benchmark, model, action = [1.00], [1.00], []
 
     for line in open("./res/log", "r").readlines():
         if line != "\n":
@@ -42,6 +51,15 @@ def main():
 
     benchmark_annualized, benchmark_returns = annualized_return(benchmark)
     model_annualized, model_returns = annualized_return(model)
+
+    benchmark_std_dev = np.std(np.array(benchmark_returns))
+    model_std_dev = np.std(np.array(model_returns))
+
+    benchmark_sharpe = benchmark_annualized / benchmark_std_dev
+    model_sharpe = model_annualized / model_std_dev
+
+    benchmark_mdd = maximum_drawdown(benchmark)
+    model_mdd = maximum_drawdown(model)
 
 if __name__ == "__main__":
     main()
